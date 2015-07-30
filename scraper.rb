434ab09b479aa536126ba2f5a6ad9516738c4f85
Tony Bowden <tony@tmtm.com>
2015-07-30 21:20:56 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,15 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'wikidata/fetcher'
+
+@pages = [
+  'Category:Members of the 31st Dáil'
+]
+
+@pages.map { |c| WikiData::Category.new(c, 'en').wikidata_ids }.flatten.uniq.each do |id|
+  data = WikiData::Fetcher.new(id: id).data('en') or next
+  puts "%s %s" % [data[:id], data[:name]]
+  # ScraperWiki.save_sqlite([:id], data)
+end
