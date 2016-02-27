65132a2639bda2d136af6835715a445dfc63fb19
Tony Bowden <tony@tmtm.com>
2016-02-27 16:37:36 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,18 +1,7 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
 
-@pages = [
-  'Category:Members of the 31st Dáil'
-]
-
-@pages.map { |c| WikiData::Category.new(c, 'en').wikidata_ids }.flatten.uniq.each do |id|
-  data = WikiData::Fetcher.new(id: id).data('en') or next
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-require 'rest-client'
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
-
+names = WikiData::Category.new( 'Category:Members of the 31st Dáil', 'en').member_titles
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
