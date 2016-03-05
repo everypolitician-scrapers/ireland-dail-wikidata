776ce159dca505321655fe6466245cf05096f458
Tony Bowden <tony@tmtm.com>
2016-03-05 13:29:51 +0000

    Fetch from page also

--- scraper.rb
+++ scraper.rb
@@ -3,6 +3,14 @@
 
 require 'wikidata/fetcher'
 
+en_names = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/wiki/Members_of_the_32nd_D%C3%A1il',
+  after: '//h2[span[@id="TDs_by_constituency"]]',
+  xpath: '//table[1]//td[position() = last() - 1]//a[not(@class="new")]/@title',
+) 
+  
 names = WikiData::Category.new( 'Category:Members of the 31st Dáil', 'en').member_titles |
-        WikiData::Category.new( 'Category:Members of the 32nd Dáil', 'en').member_titles
+        WikiData::Category.new( 'Category:Members of the 32nd Dáil', 'en').member_titles |
+        en_names
+
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
