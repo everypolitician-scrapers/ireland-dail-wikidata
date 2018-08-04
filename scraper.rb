f8ff22f47fe04a37d319739bccbe928fc21390b6
Tony Bowden <tony@tmtm.com>
2018-08-04 16:06:59 +0100

    Fetch all members via SPARQL
    All historic members should now have relevant P39s in Wikidata

--- scraper.rb
+++ scraper.rb
@@ -1,18 +1,9 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'everypolitician'
 require 'wikidata/fetcher'
 
-en_names = EveryPolitician::Wikidata.wikipedia_xpath( 
-  url: 'https://en.wikipedia.org/wiki/Members_of_the_32nd_D%C3%A1il',
-  after: '//h2[span[@id="TDs_by_party"]]',
-  xpath: '//table[1]//td[position() = last() - 1]//a[not(@class="new")]/@title',
-) 
-
-warn "Names: #{en_names.join(", ")}"
-  
-names = WikiData::Category.new( 'Category:Members of the 31st Dáil', 'en').member_titles |
-        WikiData::Category.new( 'Category:Members of the 32nd Dáil', 'en').member_titles |
-        en_names
-
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
+sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q654291 . }'
+members = EveryPolitician::Wikidata.sparql(sparq)
+EveryPolitician::Wikidata.scrape_wikidata(ids: members)
