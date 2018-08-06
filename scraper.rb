a76062b82c51fd360c0c61fdbdd9456ec5dfefe6
Tony Bowden <tony@tmtm.com>
2018-08-06 07:55:08 +0100

    Scrape in batches of 500

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,6 @@
 require 'everypolitician'
 require 'wikidata/fetcher'
 
-sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q654291 . }'
+sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q654291 }'
 members = EveryPolitician::Wikidata.sparql(sparq)
-EveryPolitician::Wikidata.scrape_wikidata(ids: members)
+EveryPolitician::Wikidata.scrape_wikidata(ids: members, batch_size: 500)
